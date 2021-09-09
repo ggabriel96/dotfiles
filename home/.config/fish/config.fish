3fa3c36b2ba5763c019fda8ede064334a82c5bf5
Gabriel Galli <ggabriel96@hotmail.com>
2021-09-09 15:01:44 -0300

    fix: update config.fish
    - set $EDITOR to kakoune
    - add ~/.config/bin to $PATH
    - remove environment variables related to homebrew
    - remove environment variables related to npm
    - remove docker-compose alias
    - add aliases for conda
    - remove gnome-keyring configuration for sway

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,30 +3,15 @@ set -gx UID (id -u)
 set -gx GID (id -g)
 set -gx CURRENT_UID "$UID:$GID"
 
-set -gx EDITOR nvim
-set -gx PATH "$PATH:$HOME/bin/"
-
-set NPM_PACKAGES "$HOME/.npm-packages/"
-set -gx PATH "$PATH:$NPM_PACKAGES/bin/"
-set -q MANPATH || set MANPATH (manpath)
-set -gx MANPATH "$MANPATH:$NPM_PACKAGES/share/man/"
-
-set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
-set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+set -gx EDITOR kak
+set -gx PATH "$PATH:$HOME/.local/bin/"
 
 # Aliases
 alias gst="git status"
 alias dps="docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Ports}}'"
-alias dcu="docker-compose up --abort-on-container-exit"
-
-# https://wiki.archlinux.org/index.php/GNOME/Keyring#Terminal_applications
-if test "$DESKTOP_SESSION" = "sway"
-    set -x (gnome-keyring-daemon --start | string split "=")
-end
+alias act="conda activate"
+alias deact="conda deactivate"
+alias new-env="conda env create -f environment.yml"
 
 if test -f "$HOME/miniconda3/bin/conda"
     eval "$HOME/miniconda3/bin/conda" "shell.fish" "hook" $argv | source
